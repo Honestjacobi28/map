e5836586bedfd3c07fe7f6e877089b74d01c0f5a
jean-fabrice OUFFOUE <96195821+Honestjacobi28@users.noreply.github.com>
2023-01-21 15:59:49 +0000

    Add files via upload

---
+++ markers/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+"""Markers view."""
+
+from django.views.generic.base import TemplateView
+
+
+class MarkersMapView(TemplateView):
+    """Markers map view."""
+
+    template_name = "map.html"
+
